ec3e807b58c8ec1f6f4d8eac9b216ae4f5207215
Bartlomiej Snochowski <bartlomiej.snochowski@gmail.com>
2022-12-23 20:03:22 +0100

    in script a little changes

--- js/script.js
+++ js/script.js
@@ -1,30 +1,27 @@
-console.log("Witam serdecznie :)");
+{
+    console.log("Witam serdecznie :)");
 
-const buttonElement = document.querySelector(".article__button");
-
-const hidePhoto = () => {
-    const myPhotoElement = document.querySelector(".article__myPhoto");
-
-    myPhotoElement.classList.toggle("hidden");
-    buttonElement.innerText = myPhotoElement.classList.contains("hidden") ? "Pokaż zdjęcie" : "Ukryj zdjęcie";
-}
-
-const initPhoto = () => {
     const buttonElement = document.querySelector(".article__button");
 
-    buttonElement.addEventListener("click", hidePhoto);
+    const hidePhoto = () => {
+        const myPhotoElement = document.querySelector(".article__myPhoto");
+
+        myPhotoElement.classList.toggle("hidden");
+        buttonElement.innerText = myPhotoElement.classList.contains("hidden") ? "Pokaż zdjęcie" : "Ukryj zdjęcie";
+    }
+
+    const backgroundColorChange = () => {
+        const container = document.querySelector(".container");
+        container.classList.toggle("newView");
+    }
+
+    const init = () => {
+        const themButtonElement = document.querySelector(".themButton");
+        themButtonElement.addEventListener("click", backgroundColorChange);
+
+        const buttonElement = document.querySelector(".article__button");
+        buttonElement.addEventListener("click", hidePhoto);
+    }
+
+    init();
 }
-
-initPhoto();
-
-const backgroundColorChange = () => {
-    const container = document.querySelector(".container");
-    container.classList.toggle("newView");
-}
-
-const initBackground = () => {
-    const themButtonElement = document.querySelector(".themButton");
-    themButtonElement.addEventListener("click", backgroundColorChange);
-}
-
-initBackground();
